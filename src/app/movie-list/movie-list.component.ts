e54a855ec4582c183140fc971e8b32caebe207b8
YU(James) DU <csjamesdu@gmail.com>
2019-12-17 22:54:50 +1100

    get posters and adjust list style

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { MoviesResponse, MoviesItem} from '../models/movies.model';
+import { MoviesResponse} from '../models/movies.model';
 import {AppHttpClient} from '../services/app-httpclient.service';
 import { timer } from 'rxjs/index';
 import {switchMap} from 'rxjs/internal/operators';
 import {MoviesItemDetailModel} from '../models/movies.detail.model';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
 import {DetailModalComponent} from '../detail-modal/detail-modal.component';
 
 const REFRESH_INTERVAL = 100000;
